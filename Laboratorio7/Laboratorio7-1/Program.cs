a769b8b2b2ccddee154ec3fea4b350bf4b5cccdf
DavidC1221 <150868134+DavidC1221@users.noreply.github.com>
2025-09-25 21:55:00 -0500

    Add files via upload

---
+++ Laboratorio7/Laboratorio7-1/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Laboratorio7_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco banco1 = new Banco();
+            banco1.Operar();
+            banco1.DepositosTotales();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratorio7/Laboratorio7-2/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Laboratorio7_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            JuegoDeDados j = new JuegoDeDados();
+            j.Jugar();
+        }
+    }
+}
