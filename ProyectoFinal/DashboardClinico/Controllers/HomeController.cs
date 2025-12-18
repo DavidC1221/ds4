d5db8ff27460747dee6d311affb4db6082a2c14d
DavidC1221 <150868134+DavidC1221@users.noreply.github.com>
2025-12-17 19:39:23 -0500

    Add files via upload

---
+++ ProyectoFinal/DashboardClinico/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+
+using System.Web.Mvc;
+
+namespace DashboardClinico.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
